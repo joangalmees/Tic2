24c8676f6e158b3e0b5c383aece991c71d40da73
joangalmees <93370856+joangalmees@users.noreply.github.com>
2021-12-10 09:21:10 +0100

    Add files via upload

---
+++ arguments1.sh
@@ -0,0 +1,10 @@
+#Fes un programa que faci la suma de dos nombres passats per argument
+
+#exercici arguments 1
+
+echo El primer argument és $1
+echo El segon és $2
+echo el nom del script és $0
+
+let suma=$1+$2
+echo "S'han sumat els arguments i el resultat es $suma"
